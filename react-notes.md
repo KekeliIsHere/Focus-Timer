6b82063c7b52fa25e9f1f4c5876f4e3bc4e7dc5c
Kekeli <agblobik@gmail.com>
2026-01-26 21:32:22 +0000

    added the hh:mm:ss format and a reset button

--- react-notes.md
+++ react-notes.md
@@ -6,3 +6,8 @@
 
 Mental model:
 State → Render → Effect → State
+
+## For the Reset and HH-MM-SS Format I added
+useRef stores initial time without rerendering.
+Derived values like minutes shouldn't be in state.
+useEffect is for side effects, not math.
--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,10 +1,28 @@
-import { useState,useEffect } from 'react'
+import { useState,useEffect,useRef } from 'react'
+
 function Timer(){
-    const [time,setTime]=useState(10);
+    const startTimeRef=useRef();
+    const [time,setTime]=useState(1500);
     const [isRunning,setIsRunning]=useState(false);
+    
+    
+    useEffect(() => {
+    // This happens once. We save '10' into our storage box.
+        startTimeRef.current = time;
+    }, []);
+    
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+    //The pad is made for the format so that time has that 02, 01, 00 look you get it.
+    const pad = (n) => String(n).padStart(2, "0");
+
+
+    
 
     useEffect(() => {
         let intervalId;
+    
 
         
         if (isRunning && time > 0) {
@@ -23,11 +41,19 @@ function Timer(){
         setIsRunning(!isRunning);   
     }  
 
+    function toggleReset(){
+        setTime(startTimeRef.current);
+        setIsRunning(false);
+    }
+
     return(
         <>
             <p>Time: {time}</p>
+            <p>Actual Time: {pad(hours)}:{pad(minutes)}:{pad(seconds)}</p>
+
             <p>Running: {isRunning?"True":"False"}</p>  
-            <button onClick={toggleIsRunning}>Start/Pause</button>      
+            <button onClick={toggleIsRunning}>Start/Pause</button> 
+            <button onClick={toggleReset}>Reset</button>     
         </>     
     );
 
