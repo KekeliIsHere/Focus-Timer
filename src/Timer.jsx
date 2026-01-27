7e12d2c7007ed22a7746eef32ba9100d621e2d19
Kekeli <agblobik@gmail.com>
2026-01-27 17:53:25 +0000

    refactored the button code to work efficiently utlizing the time as the root instead of creating new states for hours, minutes and seconds

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -2,8 +2,10 @@ import { useState,useEffect,useRef } from 'react'
 
 function Timer(){
     const startTimeRef=useRef();
-    const [time,setTime]=useState(1500);
+    const [time,setTime]=useState(0);
     const [isRunning,setIsRunning]=useState(false);
+    const MAX=59;
+    const MIN=0;
     
     
     useEffect(() => {
@@ -13,8 +15,9 @@ function Timer(){
     
     const hours = Math.floor(time / 3600);
     const minutes = Math.floor((time % 3600) / 60);
-    const seconds = time % 60;
-    //The pad is made for the format so that time has that 02, 01, 00 look you get it.
+    const seconds =time % 60;
+
+    //The pad is made for the ,format so that time has that 02, 01, 00 look you get it.
     const pad = (n) => String(n).padStart(2, "0");
 
 
@@ -35,7 +38,43 @@ function Timer(){
         return () => clearInterval(intervalId);
     }, [isRunning, time])
 
-    
+    //Added functions to increase the time with the plus and minus things.
+
+    //Hour Changes
+    const hourIncrease=()=>{
+        if(!isRunning){
+            setTime(time=>time+3600);
+        }
+    }
+    const hourDecrease=()=>{
+        setTime(time=>Math.max(0,time-3600))
+    }
+
+    //Minute Change
+    const minuteIncrease=()=>{
+        if(!isRunning){
+            setTime(time=>time+60);
+        }
+    }
+    const minuteDecrease=()=>{
+        if(!isRunning){
+            setTime(time=>Math.max(0,time-60));
+        }
+    }
+
+    //Second Change
+    const secondIncrease=()=>{
+        if(!isRunning){
+            setTime(time=>time+1);
+        }
+    }
+    const secondDecrease=()=>{
+        if(!isRunning){
+            setTime(time=>Math.max(0,time-1));
+        }
+    }
+
+
 
     function toggleIsRunning(){
         setIsRunning(!isRunning);   
@@ -51,11 +90,29 @@ function Timer(){
             <p>Time: {time}</p>
             <p>Actual Time: {pad(hours)}:{pad(minutes)}:{pad(seconds)}</p>
 
-            <p>Running: {isRunning?"True":"False"}</p>  
+            <p>Running: {isRunning?"True":"False"}</p>
+            <table>
+                <tr>
+                    <td><button onClick={hourIncrease}>+</button></td>
+                    <td><button onClick={minuteIncrease}>+</button></td>
+                    <td><button onClick={secondIncrease}>+</button></td>                    
+                </tr>
+                <tr>
+                    <td>{pad(hours)}</td>
+                    <td>{pad(minutes)}</td>
+                    <td>{pad(seconds)}</td>                                     
+                </tr>
+                <tr>
+                    <td><button onClick={hourDecrease}>-</button></td>
+                    <td><button onClick={minuteDecrease}>-</button></td>
+                    <td><button onClick={secondDecrease}>-</button></td>                    
+                </tr>
+
+            </table>
             <button onClick={toggleIsRunning}>Start/Pause</button> 
             <button onClick={toggleReset}>Reset</button>     
         </>     
     );
-
 }
+
 export default Timer;
